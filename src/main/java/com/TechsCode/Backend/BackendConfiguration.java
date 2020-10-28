540eae561d80dbefeec81d1a170f79f841f1e459
TechsCode <paul@giese.in>
2020-10-28 15:19:25 +0100

    Added BackendConfiguration + Merged with database.json

---
+++ src/main/java/com/TechsCode/Backend/BackendConfiguration.java
@@ -0,0 +1,49 @@
+package com.TechsCode.Backend;
+
+import com.TechsCode.Backend.repositories.database.DatabaseConfiguration;
+import com.TechsCode.Backend.repositories.database.DatabaseCredentials;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.commons.io.FileUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Configuration
+public class BackendConfiguration {
+
+    private static final Gson gson = new Gson();
+
+    private JsonObject content;
+
+    private BackendConfiguration() {
+        File file = new File("backend.json");
+
+        try {
+            if(!file.exists()){
+                if(file.createNewFile()){
+                    InputStream src = DatabaseConfiguration.class.getResourceAsStream("/backend.json");
+                    Files.copy(src, Paths.get(file.toURI()), StandardCopyOption.REPLACE_EXISTING);
+                }
+            }
+
+            String json = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
+            content = (JsonObject) JsonParser.parseString(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Bean
+    public DatabaseCredentials getCredentials(){
+        return gson.fromJson(content.getAsJsonObject("database"), DatabaseCredentials.class);
+    }
+}
--- src/main/java/com/TechsCode/Backend/repositories/database/DatabaseConfiguration.java
+++ src/main/java/com/TechsCode/Backend/repositories/database/DatabaseConfiguration.java
@@ -3,7 +3,6 @@ package com.TechsCode.Backend.repositories.database;
 import com.google.gson.Gson;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -13,38 +12,12 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
 @Configuration
 public class DatabaseConfiguration {
 
+    @Autowired
     private DatabaseCredentials credentials;
-
-    private DatabaseConfiguration() {
-        File file = new File("config.json");
-
-        try {
-            if(!file.exists()){
-                if(file.createNewFile()){
-                    InputStream src = DatabaseConfiguration.class.getResourceAsStream("/database.json");
-                    Files.copy(src, Paths.get(file.toURI()), StandardCopyOption.REPLACE_EXISTING);
-                }
-            }
-
-            String json = FileUtils.readFileToString(file, StandardCharsets.UTF_8);
-
-            Gson gson = new Gson();
-            credentials = gson.fromJson(json, DatabaseCredentials.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     @Bean
     @Primary
