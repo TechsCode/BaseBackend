07a8cce26f1ba10f3c7e2a1afb667f85fbd16057
TechsCode <paul@giese.in>
2020-10-29 01:57:25 +0100

    Changed DatabaseConfiguration

--- src/main/java/com/TechsCode/backend/configurations/DatabaseConfiguration.java
+++ src/main/java/com/TechsCode/backend/configurations/DatabaseConfiguration.java
@@ -2,15 +2,57 @@ package com.techscode.backend.configurations;
 
 import com.techscode.backend.configurations.beans.DatabaseCredentials;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(
+        entityManagerFactoryRef = "globalEntityManagerFactory",
+        transactionManagerRef = "globalTransactionManager",
+        basePackages = { "com.techscode.backend.entities" }
+)
+public class DatabaseConfiguration {
+
+    @Autowired
+    private DatabaseCredentials databaseCredentials;
+
+    @Bean(name = "globalDataSource")
+    public DataSource dataSource() {
+        return databaseCredentials.createDataSource("Main");
+    }
+
+    @Bean(name = "globalEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean barEntityManagerFactory(EntityManagerFactoryBuilder builder, @Qualifier("globalDataSource") DataSource dataSource) {
+        return builder
+                .dataSource(dataSource)
+                .packages("com.techscode.pluginpage.entities")
+                .persistenceUnit("global")
+                .build();
+    }
+
+    @Bean(name = "globalTransactionManager")
+    public PlatformTransactionManager barTransactionManager(@Qualifier("globalEntityManagerFactory") EntityManagerFactory factory) {
+        return new JpaTransactionManager(factory);
+    }
+
+}
+
+/*
 
 @Configuration
 public class DatabaseConfiguration {
@@ -43,3 +85,4 @@ public class DatabaseConfiguration {
     }
 
 }
+*/
