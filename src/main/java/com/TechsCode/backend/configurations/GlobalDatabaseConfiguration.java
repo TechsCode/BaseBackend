7493a026de33c617689435bc3802cab394b3ebf0
TechsCode <paul@giese.in>
2020-10-30 01:40:57 +0100

    Readded GlobalDatabaseConfiguration

--- src/main/java/com/TechsCode/backend/configurations/GlobalDatabaseConfiguration.java
+++ src/main/java/com/TechsCode/backend/configurations/GlobalDatabaseConfiguration.java
@@ -3,25 +3,18 @@ package com.techscode.backend.configurations;
 import com.techscode.backend.configurations.beans.DatabaseCredentials;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-
-/*
 
 @Configuration
 @EnableTransactionManagement
@@ -51,39 +44,6 @@ public class GlobalDatabaseConfiguration {
     @Bean(name = "globalTransactionManager")
     public PlatformTransactionManager transactionManager(@Qualifier("globalEntityManagerFactory") EntityManagerFactory db1EntityManagerFactory) {
         return new JpaTransactionManager(db1EntityManagerFactory);
-    }
-
-}*/
-
-
-@Configuration
-public class DatabaseConfiguration {
-
-    @Autowired
-    private DatabaseCredentials credentials;
-
-    @Bean
-    @Primary
-    public DataSource primaryDataSource() {
-        return credentials.createDataSource("Main");
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
-        LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
-        lef.setDataSource(dataSource);
-        lef.setJpaVendorAdapter(jpaVendorAdapter);
-        lef.setPackagesToScan("com.techscode.backend");
-        return lef;
-    }
-
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-        hibernateJpaVendorAdapter.setShowSql(true);
-        hibernateJpaVendorAdapter.setGenerateDdl(true); //Auto creating scheme when true
-        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);//Database type
-        return hibernateJpaVendorAdapter;
     }
 
 }
