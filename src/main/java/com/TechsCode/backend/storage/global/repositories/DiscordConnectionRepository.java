b7aaa5caab7bf375ebc2b4146168f42a19c52eda
TechsCode <paul@giese.in>
2020-10-31 23:01:19 +0100

    Added delete methods

--- src/main/java/com/TechsCode/backend/storage/global/repositories/DiscordConnectionRepository.java
+++ src/main/java/com/TechsCode/backend/storage/global/repositories/DiscordConnectionRepository.java
@@ -4,4 +4,7 @@ import com.techscode.backend.storage.global.entities.DiscordConnection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DiscordConnectionRepository extends JpaRepository<DiscordConnection, Integer> {
+    
+    @Override
+    public void delete(DiscordConnection discordConnection);
 }
--- src/main/java/com/TechsCode/backend/storage/global/repositories/SpigotConnectionRepository.java
+++ src/main/java/com/TechsCode/backend/storage/global/repositories/SpigotConnectionRepository.java
@@ -1,7 +1,11 @@
 package com.techscode.backend.storage.global.repositories;
 
+import com.techscode.backend.storage.global.entities.DiscordConnection;
 import com.techscode.backend.storage.global.entities.SpigotConnection;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SpigotConnectionRepository extends JpaRepository<SpigotConnection, Integer> {
+
+    @Override
+    public void delete(SpigotConnection spigotConnection);
 }
