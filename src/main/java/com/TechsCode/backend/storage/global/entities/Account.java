493671948fe4f111af24644b47856725861cb008
TechsCode <paul@giese.in>
2020-10-31 22:51:54 +0100

    Adding Getters

--- src/main/java/com/TechsCode/backend/storage/global/entities/Account.java
+++ src/main/java/com/TechsCode/backend/storage/global/entities/Account.java
@@ -40,6 +40,10 @@ public class Account {
         return id;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getEmail() {
         return email;
     }
